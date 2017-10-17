23852cc8ae4d33c7701f3f054df3eb070a179d1e
Bethany Shao <bethany.magdalena@gmail.com>
2017-10-17 13:31:17 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+
+def line(array)
+  message = ""
+  if array.size == 0
+    message = "The line is currently empty."
+  else
+    message = "The line is currently: "
+    array.each do |name|
+      message += "#{array.index(name) + 1}. name "
+    end
+  end
+  message
+end
+
+def take_a_number(array, name)
+
+end
+
+def now_serving(array)
+
+end
