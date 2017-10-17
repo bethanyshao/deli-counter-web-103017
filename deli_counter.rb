f971f2736583e5fc7624e0a4550e1ad26687eeb3
Bethany Shao <bethany.magdalena@gmail.com>
2017-10-17 13:47:08 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -24,8 +24,7 @@ def now_serving(array)
   if array.size == 0
     message = "There is nobody waiting to be served!"
   else
-    next_customer = array.shift
-    message = "Currently serving #{next_customer}."
+    message = "Currently serving #{array.shift}."
   end
   puts "#{message}"
   array
