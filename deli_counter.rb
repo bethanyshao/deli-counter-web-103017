591926e119c089f137c9bb21f210ba6a2e12a69d
Bethany Shao <bethany.magdalena@gmail.com>
2017-10-17 13:37:44 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,15 +1,16 @@
 # Write your code here.
 
 def line(array)
+  message = ""
   if array.size == 0
-    puts "The line is currently empty."
+    message = "The line is currently empty."
   else
     message = "The line is currently: "
     array.each do |name|
       message += "#{array.index(name) + 1}. name "
     end
-    puts "#{message}"
   end
+  puts "#{message}"
 end
 
 def take_a_number(array, name)
